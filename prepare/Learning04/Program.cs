29747af3f82048e11cdd2296e2f6ef66c1dfb975
Daltonbf <Daltonbf@byui.edu>
2024-02-01 21:22:19 -0700

    Learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,15 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel L. Jackson", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        MathAssignment a2 = new MathAssignment("Rob Derdeck", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        WritingAssignment a3 = new WritingAssignment("Many Waters", "Australian History", "The Causes of World War III");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
